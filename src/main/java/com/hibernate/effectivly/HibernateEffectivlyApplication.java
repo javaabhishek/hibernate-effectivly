12820e0ae7458583ee6c2b3898e5755d717226e5
javaabhishek <java.abhishek.patil@gmail.com>
2023-06-01 11:02:40 +0530

    Addedd project setup

---
+++ src/main/java/com/hibernate/effectivly/HibernateEffectivlyApplication.java
@@ -0,0 +1,13 @@
+package com.hibernate.effectivly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateEffectivlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateEffectivlyApplication.class, args);
+	}
+
+}
